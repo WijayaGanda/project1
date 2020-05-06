b82531f1a471e3eee0d9abf8e2e40c287b52dee5
WijayaGanda <64698425+WijayaGanda@users.noreply.github.com>
2020-05-06 08:31:09 +0700

    Revert "bad ending"
    This reverts commit 6ba8d7a9c3a972f76052767f3c79aba1f148bec5.

--- app/src/main/java/com/cr7/project/MainActivity.kt
+++ app/src/main/java/com/cr7/project/MainActivity.kt
@@ -3,9 +3,7 @@ package com.cr7.project
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
 import android.widget.*
-import kotlinx.android.synthetic.main.activity_halaman2.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
@@ -17,43 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val namaet = findViewById<EditText>(R.id.edtnama)
-        val jeniset = findViewById<EditText>(R.id.edtjenis)
-        val emailet = findViewById<EditText>(R.id.edtemail)
-        val telpet = findViewById<EditText>(R.id.edttelp)
-        val alamatet = findViewById<EditText>(R.id.edtalamat)
+        btnsubmit.setOnClickListener { kehalaman2() }
 
-        //ketika tombol save ditekan
-        btnsubmit.setOnClickListener {
-            //dapat dari inputan user
-            var nama = edtnama.text.toString()
-            var email = edtemail.text.toString()
-            var gender = edtjenis.text.toString()
-            var telp = edttelp.text.toString()
-            var alamat = edtalamat.text.toString()
+       }
 
-            //intent pindah halaman
-            intent = Intent(this, halaman2::class.java)
-
-            //nama untuk mengirim data ke halaman ke 2, dan untuk diambil
-
-            intent.putExtra("nama", nama)
-            intent.putExtra("email", email)
-            intent.putExtra("gender", gender)
-            intent.putExtra("telp", telp)
-            intent.putExtra("alamat", alamat)
-
-
-            startActivity(intent)
-        }
-
-
+    private fun kehalaman2() {
+        val intent = Intent(this, halaman2::class.java)
+        startActivity(intent)
     }
+
 }
-
-
-
-
-
 
 
--- app/src/main/java/com/cr7/project/halaman2.kt
+++ app/src/main/java/com/cr7/project/halaman2.kt
@@ -1,17 +1,11 @@
 package com.cr7.project
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
-import android.content.res.Configuration
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_halaman2.*
-import kotlinx.android.synthetic.main.activity_main.*
 
 class halaman2 : AppCompatActivity() {
 
@@ -20,58 +14,7 @@ class halaman2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_halaman2)
 
-        //edit nama kehalaman3
-
-        //edit profil
-        btn_ep.setOnClickListener {
-            intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-        //melakukan panggilan
-        btn_dial.setOnClickListener {
-            val number = txttel.text.toString()
-            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + Uri.encode(number)))
-            startActivity(intent)
-        }
-        //tombol profil pembuat
-        btn_pembuat.setOnClickListener {
-            intent = Intent(this, abot::class.java)
-            startActivity(intent)
-        }
-        datahalaman1()
-    }
-    //dapat data halaman1(biodata diri)
-    private fun datahalaman1(){
-        var intent = intent
-        //dapat data berdasarkan nama
-        val name = intent.getStringExtra("nama")
-        val email = intent.getStringExtra("email")
-        val gender = intent.getStringExtra("gender")
-        val telp = intent.getStringExtra("telp")
-        val alamat = intent.getStringExtra("alamat")
-
-
-
-        //tempat yang inigin diisi
-        val txt = findViewById<TextView>(R.id.txtNama)
-        val jenis = findViewById<TextView>(R.id.txtkelamin)
-        val em = findViewById<TextView>(R.id.txtemail)
-        val hp = findViewById<TextView>(R.id.txttel)
-        val address = findViewById<TextView>(R.id.txtaddress)
-
-        //isi tempat tersebut
-        txt.text = name
-        jenis.text = gender
-        em.text = email
-        hp.text = telp
-        address.text = alamat
 
     }
-
-
-
-
-
-
 
 }
